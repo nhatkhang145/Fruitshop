5f0afb6b2cdee69cf19bde8bec86eca395252bcc
23130257-NguyenTranNhatQuan <23130257@st.hcmuaf.edu.vn>
2026-01-19 00:29:57 +0700

    tao servlet duyet ma OTP

---
+++ Fruitshop_Web/src/main/java/controller/VerifyOTPServlet.java
@@ -0,0 +1,27 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "VerifyOTPServlet", urlPatterns = {"/verifyOTP"})
+public class VerifyOTPServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String enteredOTP = request.getParameter("otp");
+        HttpSession session = request.getSession();
+        String sessionOTP = (String) session.getAttribute("otp");
+
+        // Kiểm tra OTP
+        if (sessionOTP != null && sessionOTP.equals(enteredOTP)) {
+            // OTP đúng -> Chuyển sang trang đổi mật khẩu
+            response.sendRedirect("reset_pass.jsp");
+        } else {
+            request.setAttribute("error", "Mã xác thực không đúng!");
+            request.getRequestDispatcher("OTP.jsp").forward(request, response);
+        }
+    }
+}
